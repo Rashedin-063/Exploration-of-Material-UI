303095a7a4c0b3cef57c77c3a56cac1e2195168b
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-08 12:37:24 +0600

    paper component used

---
+++ Tourism-Website/src/components/TourCard.jsx
@@ -0,0 +1,11 @@
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+
+const TourCard = () => {
+  return <div>
+    <Paper elevation={3} square>
+      Hello
+    </Paper>
+  </div>;
+};
+export default TourCard;
