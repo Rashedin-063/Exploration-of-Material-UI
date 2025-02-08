25835421ed4a72dc6c88a3f8105ce770efb40b29
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-08 23:42:26 +0600

    routing done

--- Tourism-Website/src/App.jsx
+++ Tourism-Website/src/App.jsx
@@ -1,37 +1,18 @@
 import './App.css';
 import SearchAppBar from './components/Apbar';
-import TourCard from './components/TourCard';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid2';
-import cities from './data.json';
-import { Typography } from '@mui/material';
+import Home from './pages/Home';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Tour from './pages/Tour';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <SearchAppBar />
-      <Container sx={{ marginY: 5 }}>
-        {cities.map((city) => (
-          <>
-            <Typography
-              variant='h4'
-              component='h2'
-              marginTop={5}
-              marginBottom={3}
-              key={city.id}
-            >
-              Top {city.name} Tours
-            </Typography>
-
-            <Grid container spacing={5}>
-              {city.tours.map((tour) => (
-                <TourCard key={tour.id} tour={tour} />
-              ))}
-            </Grid>
-          </>
-        ))}
-      </Container>
-    </>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/:id' element={<Tour/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
