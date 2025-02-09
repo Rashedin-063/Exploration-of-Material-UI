1ce39ff82238da4660b91ff74aa83d182fc0d853
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-09 08:52:29 +0600

    tour page setup done

--- Tourism-Website/src/pages/Tour.jsx
+++ Tourism-Website/src/pages/Tour.jsx
@@ -1,6 +1,13 @@
+import { Typography } from '@mui/material';
+import Container from '@mui/material/Container';
+
 const Tour = () => {
-  return (
-    <div>Tour</div>
-  )
-}
-export default Tour
+  return <>
+    <Container sx={{ width: 900 }}>
+      <Typography variant='h3' component='h1' marginTop={3}>
+        Explore the world in Vegas
+     </Typography>
+  </Container>
+  </>;
+};
+export default Tour;
