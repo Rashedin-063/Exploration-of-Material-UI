6f0899b7deca3d7c02e83c4141cae2f48ab92363
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-09 23:42:17 +0600

    icons explored

--- Anthony-Sistilli-Playlist/src/components/Theming.jsx
+++ Anthony-Sistilli-Playlist/src/components/Theming.jsx
@@ -1,4 +1,5 @@
 import { Box, Button, Paper, Typography } from "@mui/material"
+import BackHandIcon from '@mui/icons-material/BackHand';
 
 const Theming = () => {
   return (
@@ -8,14 +9,16 @@ const Theming = () => {
         <Typography color='primary'>Hello from VS Code</Typography>
         <h3>Start editing to see some magic happen</h3>
 
-        <Button variant='contained' color='secondary'>
+        <Button variant='contained' color='secondary' endIcon={<BackHandIcon/>}>
           Hello from
         </Button>
         <Typography color='customRibRed.superLight' variant='h6'>
           Customized red text
         </Typography>
       </Paper>
-      <Box pt={3}>Hello there, it&apos;s big text</Box>
+      <Box pt={3}>Hello there, it&apos;s big text
+        <BackHandIcon color="primary" fontSize="large"/>
+      </Box>
     </Box>
   );
 }
