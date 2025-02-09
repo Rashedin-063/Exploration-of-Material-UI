938d7a110a9bf30a62d49a2958bab189a4797b66
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-09 19:46:55 +0600

    mui button explored

---
+++ Anthony-Sistilli-Playlist/src/components/ButtonContainer.jsx
@@ -0,0 +1,78 @@
+import { Box, Container, IconButton } from '@mui/material';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
+const ButtonContainer = () => {
+  return (
+    <Container>
+      <Box sx={{ display: 'flex', gap: 1 }}>
+        <Button variant='outlined'>Outlined Button</Button>
+        <Button variant='contained' size='small'>
+          Contained Small Button
+        </Button>
+        <Button variant='outlined' size='large'>
+          Small
+        </Button>
+      </Box>
+      <Box marginY={2}>
+        <Button color='success' size='small' variant='contained'>
+          Small
+        </Button>
+        <Button
+          color='secondary'
+          size='medium'
+          variant='contained'
+          href='/home'
+        >
+          Medium
+        </Button>
+        <Button color='error' size='large' variant='contained' href='/'>
+          Large
+        </Button>
+      </Box>
+      <Box>
+        <IconButton aria-label='delete'>
+          <DeleteIcon/>
+        </IconButton>
+        <Button variant="outlined" startIcon={<DeleteIcon />}>
+  Delete
+</Button>
+<Button variant="contained" endIcon={<SendIcon />}>
+  Send
+</Button>
+      </Box>
+      <Box>
+        <Button
+  component="label"
+  role={undefined}
+  variant="contained"
+  tabIndex={-1}
+  startIcon={<CloudUploadIcon />}
+>
+  Upload files
+  <VisuallyHiddenInput
+    type="file"
+    onChange={(event) => console.log(event.target.files)}
+    multiple
+  />
+</Button>
+      </Box>
+    </Container>
+  );
+};
+export default ButtonContainer;
