7b33c1c29a966fe372e66ff2ccbd0290c344f1af
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-09 09:26:33 +0600

    image collage added

--- Tourism-Website/src/pages/Tour.jsx
+++ Tourism-Website/src/pages/Tour.jsx
@@ -1,13 +1,41 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import Container from '@mui/material/Container';
+import ImageCollage from '../components/ImageCollage';
 
 const Tour = () => {
-  return <>
-    <Container sx={{ width: 900 }}>
-      <Typography variant='h3' component='h1' marginTop={3}>
-        Explore the world in Vegas
-     </Typography>
-  </Container>
-  </>;
+  return (
+    <>
+      <Container sx={{ width: 900 }}>
+        <Typography variant='h3' component='h1' marginTop={3}>
+          Explore the world in Vegas
+        </Typography>
+        <Box marginTop={3} sx={{ display: 'flex' }}>
+          <img
+            src='https://media.timeout.com/images/105124791/750/422/image.jpg'
+            height={325}
+            style={{ borderRadius: '4px' }}
+          />
+          <ImageCollage />
+        </Box>
+        <Box>
+          <Typography variant='h6' component='h4' marginTop={3}>
+           About this ticket
+          </Typography>
+          <Typography variant='p' component='p' marginTop={3}>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dolore consequuntur explicabo earum perspiciatis eaque, error sint blanditiis maxime culpa, aliquid nobis accusantium repellat dolores. Asperiores dolorum sapiente numquam, vitae repellat perspiciatis modi, nobis nemo autem error tempora minima nihil?
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant='h6' component='h4' marginTop={3}>
+           About this ticket
+          </Typography>
+          <Typography variant='p' component='p' marginTop={3}>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dolore consequuntur explicabo earum perspiciatis eaque, error sint blanditiis maxime culpa, aliquid nobis accusantium repellat dolores. Asperiores dolorum sapiente numquam, vitae repellat perspiciatis modi, nobis nemo autem error tempora minima nihil?
+          </Typography>
+        </Box>
+        
+      </Container>
+    </>
+  );
 };
 export default Tour;
