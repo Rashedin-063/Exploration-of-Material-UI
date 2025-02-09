fed6f9aaeeeeb319e88bcf271eb9e74e6e86d14b
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-09 10:03:41 +0600

    footer done with bottom-navigation, modal and date-picker explored

--- Tourism-Website/src/pages/Tour.jsx
+++ Tourism-Website/src/pages/Tour.jsx
@@ -1,7 +1,8 @@
-import { Box, Typography } from '@mui/material';
+import { BottomNavigation, Box, Paper, Typography } from '@mui/material';
 import Container from '@mui/material/Container';
 import ImageCollage from '../components/ImageCollage';
 import CustomizedAccordions from '../components/Accordion';
+import BasicModal from '../components/Modal';
 
 const Tour = () => {
   return (
@@ -21,20 +22,33 @@ const Tour = () => {
         {/* title and description */}
         <Box>
           <Typography variant='h6' component='h4' marginTop={3}>
-           About this ticket
+            About this ticket
           </Typography>
           <Typography variant='p' component='p' marginTop={3}>
-           Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dolore consequuntur explicabo earum perspiciatis eaque, error sint blanditiis maxime culpa, aliquid nobis accusantium repellat dolores. Asperiores dolorum sapiente numquam, vitae repellat perspiciatis modi, nobis nemo autem error tempora minima nihil?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+            dolore consequuntur explicabo earum perspiciatis eaque, error sint
+            blanditiis maxime culpa, aliquid nobis accusantium repellat dolores.
+            Asperiores dolorum sapiente numquam, vitae repellat perspiciatis
+            modi, nobis nemo autem error tempora minima nihil?
           </Typography>
         </Box>
         {/* accordion */}
-        <Box>
+        <Box marginBottom={10}>
           <Typography variant='h6' component='h4' marginY={3}>
-           Frequently Asked Question
+            Frequently Asked Question
           </Typography>
-    <CustomizedAccordions/>
+          <CustomizedAccordions />
+          <Paper
+            sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
+            elevation={3}
+          >
+            <BottomNavigation
+              showLabels
+            >
+          <BasicModal/>
+            </BottomNavigation>
+          </Paper>
         </Box>
-        
       </Container>
     </>
   );
