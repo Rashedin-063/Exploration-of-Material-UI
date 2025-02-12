a632efeb8efe25b9c1ad5f28a717f51073391d76
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-12 12:03:30 +0600

    dynamic progress explored

--- Anthony-Sistilli-Playlist/src/components/LoadingSpinner.jsx
+++ Anthony-Sistilli-Playlist/src/components/LoadingSpinner.jsx
@@ -1,13 +1,43 @@
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 import { Stack } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 export default function CircularIndeterminate() {
+
+  const [progress, setProgress] = useState(0)
+
+// useEffect(() => {
+//   const timer = setInterval(() => {
+//     setProgress((prevProgress) =>
+//       prevProgress >= 100 ? 0 : prevProgress + 10
+//     );
+//   }, 800);
+
+//   return () => {
+//     clearInterval(timer);
+//   };
+  // }, []);
+  
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress(prevState => {
+        if (prevState >= 100) return 0;
+        else {
+          return ( prevState + 10);
+        }
+      })
+    }, 800)
+
+    return () => { clearTimeout(timer)}
+  },[])
+
+
   return (
     <Box sx={{ display: 'flex', gap: 4 }}>
-      <CircularProgress />
+      {/* <CircularProgress />
       <CircularProgress color='secondary' />
-      <CircularProgress color='primary' />
+      <CircularProgress color='primary' /> */}
       {/* <Stack sx={{ color: 'grey.500' }} spacing={2} direction='row'>
         <CircularProgress color='secondary' />
         <CircularProgress color='success' />
@@ -19,6 +49,7 @@ export default function CircularIndeterminate() {
         <CircularProgress size='3rem' />
         <CircularProgress variant='determinate' value={25} />
       </Box> */}
+      <CircularProgress variant='determinate' value={progress} />
     </Box>
   );
 }
