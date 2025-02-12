b4a128423acc29e1de20d6e40228fd990792da86
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-12 14:01:54 +0600

    linear progress explored

--- Anthony-Sistilli-Playlist/src/App.jsx
+++ Anthony-Sistilli-Playlist/src/App.jsx
@@ -2,19 +2,30 @@
 // import TypographyContainer from './components/TypographyContainer';
 
 // import SxProp from "./components/SxProp";
+import CircularIntegration from "./components/InteractiveProgress";
+import LinearIndeterminate from "./components/LinearProgress";
 import CircularIndeterminate from "./components/LoadingSpinner";
-import Theming from "./components/Theming";
+// import Theming from "./components/Theming";
 
 
 const App = () => {
   return (
-    <div style={{minHeight: '80vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+    <div
+      style={{
+        minHeight: '80vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       {/* <ButtonContainer />
       <TypographyContainer/> */}
       {/* <SxProp/> */}
       {/* <Theming/> */}
-      <CircularIndeterminate/>
-      
+      {/* <CircularIndeterminate/> */}
+      <CircularIntegration />
+      <LinearIndeterminate/>
     </div>
   );
 };
