004e4874f8567b9c9dbd1c06033eb14dbf735da5
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-16 00:07:58 +0600

    transfer list explored

--- Anthony-Sistilli-Playlist/src/components/TransferList/TransfarListContainer.jsx
+++ Anthony-Sistilli-Playlist/src/components/TransferList/TransfarListContainer.jsx
@@ -1,6 +1,13 @@
+import { Stack } from "@mui/material"
+import SelectAllTransferList from "./SelectAllTransferList"
+import TransferList from "./TransferList"
+
 const TransfarListContainer = () => {
   return (
-    <div>TransfarListContainer</div>
+    <Stack spacing={4}>
+      <TransferList />
+      <SelectAllTransferList/>
+    </Stack>
   )
 }
 export default TransfarListContainer
