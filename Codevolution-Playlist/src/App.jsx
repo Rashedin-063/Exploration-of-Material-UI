4edb4da70d6c955b73e24f1677b0eff32718a981
Rashedin-063 <rashedinislam.06@gmail.com>
2025-02-22 11:00:34 +0600

    codevolution playlist added

---
+++ Codevolution-Playlist/src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <main
+      style={{
+        minHeight: '80vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 10,
+      }}
+    >
+      App
+    </main>
+  );
+}
+export default App
